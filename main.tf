d6eec9d19b160e76c677846a8231a28130a04b62
Sanjay Tiwari <learnbyexample01234@gmail.com>
2020-05-10 16:48:59 -0400

    Adding network and fixing code

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
 provider "google" {
-  project     = 
-  
+  project     = "learning-project-276016"
+    region      = "us-east1"
 }
 resource "google_compute_instance" "default" {
-  name         = "test"
+  name         = "test2"
   machine_type = "n1-standard-1"
   zone         = "us-east1-b"
-  region      = "us-east1"
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
     }
+  }
+    network_interface{
+    network = "default"
   }
 }
